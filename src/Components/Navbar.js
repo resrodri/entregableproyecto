2d8d4997683894ad30c273b055f1a0b370d5ab91
resrodri <resrodri1@gmail.com>
2022-07-04 23:56:41 -0300

    Menu E-Commerce

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return <div>
+                <img src="logo.jpg"></img>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Productos</a></li>
+                    <li><a href="#">Galeria</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+    </div>;
+};
+export default "Navbar";
+
